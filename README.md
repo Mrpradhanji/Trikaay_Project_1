5e3053e9b6d6e766eafb7a5563b98c902a8bc880
Mrpradhanji <merohitsingh3@gmail.com>
2025-06-21 13:02:05 +0530

    set up

--- README.md
+++
@@ -1,1 +0,0 @@
-# Trikaay_Project_1
---
+++ trikaay/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500 font-bold underline">Hello World - Updated!</h1>
+   
+       
+    </div>
+  );
+}
